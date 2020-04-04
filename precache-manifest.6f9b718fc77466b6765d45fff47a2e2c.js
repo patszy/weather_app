01b5aee45ed253dc88251e786a565f294de7ba73
patszy <patszy97@interia.pl>
2020-04-04 14:36:22 +0200

    Updates

---
+++ precache-manifest.6f9b718fc77466b6765d45fff47a2e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e18624666f4dee2e4050d9647dfcb0b",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "4c767de1237a4861e106",
+    "url": "/weather_app/static/css/main.5e8b7dfa.chunk.css"
+  },
+  {
+    "revision": "ff33d1e9e1bc8a9acb45",
+    "url": "/weather_app/static/js/2.8d6af592.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather_app/static/js/2.8d6af592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c767de1237a4861e106",
+    "url": "/weather_app/static/js/main.96871f65.chunk.js"
+  },
+  {
+    "revision": "591e2505f6ee8d973312",
+    "url": "/weather_app/static/js/runtime-main.207e6037.js"
+  }
+]);
